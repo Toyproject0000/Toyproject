542028c3c802608a59bb25e37d47cb0f78bfabed
minwung <alsdnd336@naver.com>
2023-06-16 12:47:07 +0900

    Writing textformfield

--- lib/main_page/4_page/writing_page.dart
+++ lib/main_page/4_page/writing_page.dart
@@ -13,80 +13,84 @@ class _WritingPageState extends State<WritingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric( horizontal: 10),
-            decoration: BoxDecoration(
-              border: Border.symmetric(
-                horizontal: BorderSide(
-                  color: Colors.grey,
-                  width: 1.0
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 10,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  border: Border.symmetric(
+                    horizontal: BorderSide(
+                      color: Colors.grey
+                    ),
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Icon(Icons.image, size: 35,),
+                        Text('사진')
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.density_large, size: 35,),
+                        Text('구분선'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.format_bold, size: 35,),
+                        Text('굵기'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.format_size, size: 35,),
+                        Text('크기'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.format_list_numbered, size: 35,),
+                        Text('번호'),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Icon(Icons.format_bold, size: 35,),
+                        Text('굵기'),
+                      ],
+                    ),
+                  ],
                 ),
               ),
-            ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: '제목을 입력하시오',
-                focusedBorder: InputBorder.none,
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: '제목을 입력하시오',
+                  focusedBorder: InputBorder.none,
+                  hintStyle: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 30
+                ),
               ),
-            )
+              SizedBox(height: 5,),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: '제목을 입력하시오',
+                  focusedBorder: InputBorder.none,
+                ),
+              ),
+            ],
           ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
-            decoration: BoxDecoration(
-                border: Border.all(
-                    color: Colors.grey,
-                    width: 2.0
-                ),
-                borderRadius: BorderRadius.circular(5)
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    Icon(Icons.image, size: 35,),
-                    Text('사진')
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.density_large, size: 35,),
-                    Text('구분선'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.format_bold, size: 35,),
-                    Text('굵기'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.format_size, size: 35,),
-                    Text('크기'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.format_list_numbered, size: 35,),
-                    Text('번호'),
-                  ],
-                ),
-                Column(
-                  children: [
-                    Icon(Icons.format_bold, size: 35,),
-                    Text('굵기'),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
