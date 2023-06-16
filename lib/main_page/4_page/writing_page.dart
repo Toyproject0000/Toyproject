e5b806dafc9d1f25928a95b397ade8736710760d
minwung <alsdnd336@naver.com>
2023-06-16 15:28:19 +0900

    Bofore stack

--- lib/main_page/4_page/writing_page.dart
+++ lib/main_page/4_page/writing_page.dart
@@ -13,82 +13,138 @@ class _WritingPageState extends State<WritingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 10,),
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                decoration: BoxDecoration(
-                  border: Border.symmetric(
-                    horizontal: BorderSide(
-                      color: Colors.grey
+      body: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.cancel_outlined,
+                          size: 40,
+                        ),
+                      ),
+                      Text(
+                        '새 게시물',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          '다음',
+                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  child: TextFormField(
+                    maxLength: 20,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      hintText: '제목',
+                      focusedBorder: InputBorder.none,
+                      counterText: '',
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
                     ),
                   ),
                 ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      children: [
-                        Icon(Icons.image, size: 35,),
-                        Text('사진')
-                      ],
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                    border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.grey),
                     ),
-                    Column(
-                      children: [
-                        Icon(Icons.density_large, size: 35,),
-                        Text('구분선'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.format_bold, size: 35,),
-                        Text('굵기'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.format_size, size: 35,),
-                        Text('크기'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.format_list_numbered, size: 35,),
-                        Text('번호'),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Icon(Icons.format_bold, size: 35,),
-                        Text('굵기'),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: '제목을 입력하시오',
-                  focusedBorder: InputBorder.none,
-                  hintStyle: TextStyle(
-                    fontSize: 30,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.image,
+                          size: 35,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.density_large,
+                          size: 35,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.format_bold,
+                          size: 35,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.format_size,
+                          size: 35,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.emoji_symbols_rounded,
+                          size: 35,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: (){},
+                        icon: Icon(
+                          Icons.color_lens,
+                          size: 35,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                style: TextStyle(
-                  fontSize: 30
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 5),
+                  child: TextFormField(
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      hintText: '본문',
+                      focusedBorder: InputBorder.none,
+                      enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(height: 5,),
-              TextFormField(
-                decoration: InputDecoration(
-                  hintText: '제목을 입력하시오',
-                  focusedBorder: InputBorder.none,
-                ),
-              ),
-            ],
+
+              ],
+            ),
           ),
         ),
       ),
