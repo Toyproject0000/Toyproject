8588b48fd5b36518678962c358ae5f427cd7ec80
kwh <kwh871005@gmail.com>
2023-06-25 16:13:37 +0900

    [Feat] 23.06.25

---
+++ src/main/java/toyproject/demo/Config.java
@@ -0,0 +1,17 @@
+package toyproject.demo;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class Config implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("도메인1", "http://localhost:8080")//도메인1: flutter 도메인 주소
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type");
+    }
+}
