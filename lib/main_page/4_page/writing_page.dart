73a6c934e6c52660676184d8a2236372f02d8b02
minwung <alsdnd336@naver.com>
2023-06-28 19:25:25 +0900

    showMenu

--- lib/main_page/4_page/writing_page.dart
+++ lib/main_page/4_page/writing_page.dart
@@ -21,38 +21,16 @@ class _WritingPageState extends State<WritingPage> {
   bool usertouch = false;
   double? containerHeight;
   File? imagePath;
-  bool barActivation = false;
+  bool barActivation = true;
   FocusNode? newFocusNode;
   int? widgetListIndex;
+  bool toolbarbool = true;
+  ScrollController scrollController = ScrollController();
+  BuildContext? showContext;
 
 
   final GlobalKey _widgetKey = GlobalKey();
   TextStyle basicFont = TextStyle(fontSize: 15, color: Colors.black);
-
-  void showImageMenu(BuildContext context) {
-    final RenderBox overlay =
-    Overlay.of(context)!.context.findRenderObject() as RenderBox; // 더 공부하기
-    final RenderBox widgetBox =
-    _widgetKey.currentContext!.findRenderObject() as RenderBox;
-    final Offset widgetPosition =
-    widgetBox.localToGlobal(Offset.zero); // 상대 위치를 절대 위치로 변경
-
-    final List<PopupMenuEntry> menuItems = [
-      PopupMenuItem(child: Text('사진 찍기')),
-      PopupMenuItem(child: Text('갤러리에서 가져오기')),
-      PopupMenuItem(child: Text('파일 선택')),
-    ];
-
-    showMenu(
-        context: context,
-        position: RelativeRect.fromLTRB(
-          widgetPosition.dx, // 왼쪽
-          widgetPosition.dy + widgetBox.size.height, // 위쪽
-          overlay.size.width - widgetPosition.dx - widgetBox.size.width, // 오른쪽
-          overlay.size.height - widgetPosition.dy, // 아래쪽
-        ),
-        items: menuItems);
-  }
 
   Future<void> pickImage() async {
     final picker = ImagePicker();
@@ -66,200 +44,67 @@ class _WritingPageState extends State<WritingPage> {
   }
 
   void removeWidget(int index) {
+    // 줄바꿈에서 활용하기
     setState(() {
       if (widgetList.length > 1) {
         widgetList.removeAt(index);
       }
     });
-    // Future.delayed(Duration(milliseconds: 100), () {
-    //   _scrollController.animateTo(
-    //     _scrollController.position.maxScrollExtent,
-    //     duration: Duration(milliseconds: 300),
-    //     curve: Curves.easeInOut,
-    //   );
-    //   // 새로운 EditableText에 focus 설정을 위해 잠시 지연시간을 줍니다.
-    //   FocusScope.of(context).requestFocus();
-    // });
   }
 
-  void addLine(context) {
-    final TextEditingController newController = TextEditingController();
-    final FocusNode newFocusNode = FocusNode();
-    final GlobalKey lineKey = GlobalKey();
+  void showSortingMenu(BuildContext context) {
+    final RenderBox overlay =
+    Overlay.of(context).context.findRenderObject() as RenderBox; // 더 공부하기
+    final RenderBox widgetBox =
+    _widgetKey.currentContext!.findRenderObject() as RenderBox;
+    final Offset widgetPosition =
+    widgetBox.localToGlobal(Offset.zero); // 상대 위치를 절대 위치로 변경
 
-    showLineMenu(
-        BuildContext context,
-        ) {
-      final RenderBox overlay = Overlay.of(context)!.context.findRenderObject()
-      as RenderBox; // 더 공부하기
-      final RenderBox widgetBox =
-      lineKey.currentContext!.findRenderObject() as RenderBox;
-      final Offset widgetPosition =
-      widgetBox.localToGlobal(Offset.zero); // 로컬에서 젼역으로 변경하는 코드
-
-      final List<PopupMenuEntry> menuItems = [
-        PopupMenuItem(
-          child: Row(
-            children: [
-              IconButton(
-                icon: Icon(Icons.linear_scale_outlined),
-                onPressed: () {
-                  setState(() {
-                    barActivation = false;
-                    Navigator.pop(context);
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.line_axis_outlined),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.line_weight_sharp),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ],
-          ),
-        )
-      ];
-      // setState(() {
-      //   barActivation = true;
-      //   showMenu(
-      //       context: context,
-      //       position: RelativeRect.fromLTRB(
-      //         widgetPosition.dx, // 왼쪽
-      //         widgetPosition.dy + widgetBox.size.height, // 위쪽
-      //         overlay.size.width -
-      //             widgetPosition.dx -
-      //             widgetBox.size.width, // 오른쪽
-      //         overlay.size.height - widgetPosition.dy, // 아래쪽
-      //       ),
-      //       items: menuItems);
-      // });
-    }
-
-    setState(() {
-      widgetList.add(GestureDetector(
-        key: lineKey,
-        onTap: () {
-          showLineMenu(context);
-        },
-        child: RawKeyboardListener(
-          focusNode: FocusNode(),
-          onKey: (event) {
-            if (event is RawKeyDownEvent &&
-                event.logicalKey == LogicalKeyboardKey.backspace) {}
-          },
-          child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            child: Image.asset(
-              'image/line1.png',
-            ),
-          ),
-        ),
-      ));
-    });
-    Future.delayed(Duration(milliseconds: 100), () {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-      // 새로운 EditableText에 focus 설정을 위해 잠시 지연시간을 줍니다.
-      FocusScope.of(context).requestFocus(newFocusNode);
-    });
-  }
-
-
-  void addWidgetToColumn() {
-    final TextEditingController newController = TextEditingController();
-    final FocusNode newFocusNode = FocusNode();
-
-    setState(() {
-      widgetControllers.add(newController);
-      widgetFocustNodes.add(newFocusNode);
-      String previousText = '';
-      RawKeyboardListener? newWidget;
-
-      newWidget = RawKeyboardListener(
-        focusNode: FocusNode(),
-        onKey: (event) {
-          String currentText = newController.text;
-          int index = widgetList.indexOf(newWidget!);
-          if (currentText.isEmpty && event is RawKeyDownEvent &&
-              event.logicalKey == LogicalKeyboardKey.backspace) {
-            removeWidget(index);
-          }
-        },
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 3),
-          child: EditableText(
-            keyboardType: TextInputType.text,
-            backgroundCursorColor: Colors.black,
-            cursorColor: Colors.black,
-            onEditingComplete: () {
-              print('함수가 실행됬다.');
-              addWidgetToColumn();
+    final List<PopupMenuEntry> menuItems = [
+      PopupMenuItem(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(onTap: (){
+              Navigator.pop(context);
             },
-            onChanged: (text){
-              if(text.contains('\n')){
-                newController.text = lineChange();
-                addWidgetToColumn();
-              }
-            },
-            controller: newController,
-            focusNode: newFocusNode,
-            style: basicFont,
-            maxLines: null,
-          ),
+                child: Image.asset('image/left-align.png', width: 25, height: 25,)),
+            SizedBox(width:10),
+            GestureDetector(onTap: (){
+              Navigator.pop(context);
+            }, child: Image.asset('image/format.png', width: 25, height: 25,)),
+            SizedBox(width:10),
+            GestureDetector(onTap: (){
+              Navigator.pop(context);
+            }, child: Image.asset('image/align-right.png', width: 25, height: 25,))
+          ],
         ),
-      );
+      )
+    ];
 
-      widgetList.add(newWidget!);
-    });
-    Future.delayed(Duration(milliseconds: 100), () {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-      // 새로운 EditableText에 focus 설정을 위해 잠시 지연시간을 줍니다.
-      FocusScope.of(context).requestFocus(newFocusNode);
-    });
+    showMenu(
+        context: context,
+        position: RelativeRect.fromLTRB(
+          widgetPosition.dx, // 왼쪽
+          widgetPosition.dy + widgetBox.size.height, // 위쪽
+          overlay.size.width - widgetPosition.dx - widgetBox.size.width, // 오른쪽
+          overlay.size.height - widgetPosition.dy, // 아래쪽
+        ),
+        items: menuItems);
   }
 
   List<Widget> widgetList = [];
   List<TextEditingController> widgetControllers = [];
   List<FocusNode> widgetFocustNodes = [];
+  List<ScrollController> widgetScrollController = [];
   List<Widget> everyThing = [];
+  List<Widget> toolbar = [];
 
-  lineChange() {
-    String currentText = bodyController.text.replaceAll('\n', '');
-    print('줄바껴써용~');
-    return currentText;
-  }
-
-  Widget BuildWidget(int index){
-    return RawKeyboardListener(
-        onKey: (event) {
-          String currentText = widgetControllers[index].text;
-          if (currentText.isEmpty &&
-              event is RawKeyDownEvent &&
-              event.logicalKey == LogicalKeyboardKey.backspace) {
-            removeWidget(index);
-          }
-        },
-        focusNode: widgetFocustNodes[index],
-        child: widgetList[index]);
-  }
 
   @override
   void initState() {
     super.initState();
+
     widgetFocustNodes.add(_focusNode);
     widgetControllers.add(bodyController);
     widgetList = [
@@ -269,67 +114,119 @@ class _WritingPageState extends State<WritingPage> {
         style: basicFont,
         cursorColor: Colors.black,
         backgroundCursorColor: Colors.black,
-        maxLines: null,
-        onEditingComplete: () {
-          print('함수가 실행됬다.');
-          addWidgetToColumn();
-        },
-        onChanged: (text) {
-          if (text.contains('\n')) {
-            bodyController.text = lineChange();
-            addWidgetToColumn();
-          }
-        },
+        maxLines: 1,
       ),
     ];
   }
 
   @override
-  Widget build(BuildContext context) {
-    List<Widget> toolbar = [
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    toolbar = [
+      Container(
+        decoration: BoxDecoration(
+          border: Border(
+              right: BorderSide(
+                color: Colors.grey,
+                width: 1,
+              )),
+        ),
+        child: IconButton(
+          tooltip: '구분선',
+          onPressed: () {
+            // addLine(); 구분선 넣기
+          },
+          icon: Icon(
+            Icons.density_large,
+            size: 35,
+          ),
+        ),
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      GestureDetector(
+        onTap: () {},
+        child: Text('글꼴'),
+      ),
+      SizedBox(
+        width: 20,
+      ),
+      GestureDetector(
+        onTap: () {},
+        child: Text(basicFont.fontSize.toString()),
+      ),
+      SizedBox(
+        width: 10,
+      ),
       IconButton(
-        tooltip: '배경이미지',
+        tooltip: '굵기',
+        onPressed: () {
+          int index = 4;
+        },
+        icon: Icon(
+          Icons.format_bold,
+          size: 35,
+        ),
+      ),
+      IconButton(
+        tooltip: '기울임',
+        onPressed: () {
+          int index = 4;
+        },
+        icon: Icon(
+          Icons.format_italic,
+          size: 35,
+        ),
+      ),
+      TextButton(
+        onPressed: () {},
+        child: Image.asset('image/underline.png',
+          width: 25, height: 25,
+        ),
+      ),
+      TextButton(
+        onPressed: () {},
+        child: Image.asset('image/strikethrough.png',
+          width: 30, height: 30,
+        ),
+      ),
+      TextButton(
+        onPressed: () {},
+        child: Image.asset('image/font.png',
+          width: 30, height: 30,
+        ),
+      ),
+      TextButton(
+        onPressed: () {},
+        child: Image.asset('image/text.png',
+          width: 30, height: 30,
+        ),
+      ),
+      Container(
         key: _widgetKey,
-        onPressed: () {
-          int index = 0;
-          showImageMenu(context);
-        },
-        icon: Icon(
-          Icons.image,
-          size: 35,
+        decoration: BoxDecoration(
+          border: Border(
+              left: BorderSide(
+                color: Colors.grey,
+                width: 1,
+              )),
         ),
-      ),
-      IconButton(
-        tooltip: '구분선',
-        onPressed: () {
-          addLine(context);
-        },
-        icon: Icon(
-          Icons.density_large,
-          size: 35,
-        ),
-      ),
-      IconButton(
-        tooltip: '택스트 스타일',
-        onPressed: () {
-          int index = 2;
-        },
-        icon: Icon(
-          Icons.format_size,
-          size: 35,
-        ),
-      ),
-      IconButton(
-        tooltip: '배경색',
-        onPressed: () {
-          int index = 3;
-        },
-        icon: Icon(
-          Icons.color_lens,
-          size: 35,
+        child: TextButton(
+          onPressed: () {
+            showSortingMenu(context);
+          },
+          child: Image.asset('image/left-align.png',
+            width: 30, height: 30,
+          ),
         ),
       ),
     ];
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
 
     everyThing = [
       Container(
@@ -388,9 +285,12 @@ class _WritingPageState extends State<WritingPage> {
             horizontal: BorderSide(color: Colors.grey),
           ),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: toolbar,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: toolbar,
+          ),
         ),
       ),
       Column(
