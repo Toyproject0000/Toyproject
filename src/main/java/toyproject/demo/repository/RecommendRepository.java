bf2603e2859fa9d9cb518fa2668e260e62b6e059
kwh <kwh871005@gmail.com>
2023-08-06 22:12:38 +0900

    [Feat] 23.08.06

--- src/main/java/toyproject/demo/repository/RecommendRepository.java
+++
@@ -1,9 +0,0 @@
-package toyproject.demo.repository;
-
-import toyproject.demo.domain.Post;
-
-import java.util.List;
-
-public interface RecommendRepository {
-    public String recommend(String userId);
-}
