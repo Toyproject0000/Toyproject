082ce642e1c8f7c31faf735a9767383009f17665
kwh <kwh871005@gmail.com>
2023-09-02 23:38:03 +0900

    [Feat] 23.09.01

--- src/main/java/toyproject/demo/service/UserService.java
+++ src/main/java/toyproject/demo/service/UserService.java
@@ -77,7 +77,6 @@ public class UserService {
         if (!findUser.get(0).getPassword().equals(user.getPassword())){
             return "비번 오류";
         }
-        if (findUser.get(0).getNickname().equals("")) return "닉네임 설정 안됨";
 
         return ",\n\"id\" : \""+findUser.get(0).getId()+"\",\n\"nickname\" : \""+findUser.get(0).getNickname()+"\"}";
     }
@@ -90,7 +89,6 @@ public class UserService {
         if (!findUser.get(0).getRoot().equals(user.getRoot())){
             return "잘못된 접근입니다.";
         }
-        if (findUser.get(0).getNickname().equals("")) return "닉네임 설정 안됨";
 
         return ",\n\"id\" : \""+findUser.get(0).getId()+"\",\n\"nickname\" : \""+findUser.get(0).getNickname()+"\"}";
     }
