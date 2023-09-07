a638d72bf9ff95d2693ab06a7f656778ebb3e62e
kwh <kwh871005@gmail.com>
2023-09-07 20:57:48 +0900

    [Feat] 23.09.07

--- src/main/java/toyproject/demo/howtouse/ReplyLikeControllerHowToUse.java
+++ src/main/java/toyproject/demo/howtouse/ReplyLikeControllerHowToUse.java
@@ -1,17 +1,11 @@
 package toyproject.demo.howtouse;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import toyproject.demo.converter.ReplyLikeConverter;
-import toyproject.demo.domain.DTO.ReplyLikeWithTokenDTO;
-import toyproject.demo.domain.ReplyLike;
-import toyproject.demo.service.JwtTokenUtil;
-import toyproject.demo.service.ReplyLikeService;
 
 @Controller
 @RequestMapping("/reply-like")
-public class ReplyLikeController {
+public class ReplyLikeControllerHowToUse {
 
     @GetMapping("/add")
     public String add(){
